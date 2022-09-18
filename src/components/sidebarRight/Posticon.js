8aa3966489dc1ce2fae58d742176dcec09f27892
MauricioSerrato <mserato99@gmail.com>
2022-09-17 19:18:32 -0500

    post on right side bar set

--- src/components/sidebarRight/Posticon.js
+++ src/components/sidebarRight/Posticon.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-export default function Posticon({setShowIcon}) {
-
-const toggleIconOff = () => {
-    setShowIcon= (false)
+export default function Posticon({setShowIcon, setIcon}) {
 
 
-}
+const toggleOff = () => {
 
+    setIcon(false)
+
+}    
 	return( 
-        <dh-component>
+        <dh-component  >
             
-            <div class="py-12 bg-gray-700 transition duration-150 ease-in-out z-10 absolute top-0 right-0 bottom-0 left-0" id="modal">
+            <div class="py-12 bg-yellow-300 transition duration-150 ease-in-out z-10 absolute top-0 right-0 bottom-0 left-0" id="modalpost">
                 <div role="alert" class="container mx-auto w-11/12 md:w-2/3 max-w-lg">
                     <div class="relative py-8 px-5 md:px-10 bg-white shadow-md rounded border border-gray-400">
                         <div class="w-full flex justify-start text-gray-600 mb-3">
@@ -65,7 +65,7 @@ const toggleIconOff = () => {
                         </div> */}
                         <div class="flex items-center justify-start w-full">
                             <button class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 bg-indigo-700 rounded text-white px-8 py-2 text-sm">Submit</button>
-                            <button class="focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-gray-400 ml-3 bg-gray-100 transition duration-150 text-gray-600 ease-in-out hover:border-gray-400 hover:bg-gray-300 border rounded px-8 py-2 text-sm" onclick={toggleIconOff}>Cancel</button>
+                            <button class="focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-gray-400 ml-3 bg-gray-100 transition duration-150 text-gray-600 ease-in-out hover:border-gray-400 hover:bg-gray-300 border rounded px-8 py-2 text-sm" onClick={toggleOff} >Cancel</button>
                         </div>
                         <button class="cursor-pointer absolute top-0 right-0 mt-4 mr-5 text-gray-400 hover:text-gray-600 transition duration-150 ease-in-out rounded focus:ring-2 focus:outline-none focus:ring-gray-600" onclick="modalHandler()" aria-label="close modal" role="button">
                             <svg  xmlns="http://www.w3.org/2000/svg"  class="icon icon-tabler icon-tabler-x" width="20" height="20" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -111,6 +111,6 @@ const toggleIconOff = () => {
                     })();
                 }
             </script> */}
-            
+     
     </dh-component>
     )}

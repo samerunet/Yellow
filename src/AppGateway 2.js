0ab4786f45ad5793eb90f64b812ffa756a8e622c
samerunet <samerunet@gmail.com>
2022-09-18 12:47:51 -0700

    login signin navbar, users left== completed

--- src/AppGateway 2.js
+++ src/AppGateway 2.js
@@ -1,11 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/login/Login.js";
-import Signup from "./components/login/Signup.js";
 import { useState } from "react";
 import Main from "./components/Main.js";
 import Nav from "./components/navbar/Navbar.js";
-import New from "./components/sidebarRight/Posticon";
 
 const ProtectedRoute = ({ permission, children }) => {
 	if (!permission) {
@@ -20,7 +18,6 @@ export default function AppGateway() {
 	const [post, setPost] = useState([]);
 	const [comment, setComment] = useState([]);
 	const [permission, setPermission] = useState(false);
-	const API = "https://secure-forest-62515.herokuapp.com/api";
 
 	return (
 		<BrowserRouter>
@@ -29,10 +26,6 @@ export default function AppGateway() {
 				<Route
 					path='/login'
 					element={<Login setUser={setUser} setPermission={setPermission} />}
-				/>
-				<Route
-					path='/signup'
-					element={<Signup setPermission={setPermission} />}
 				/>
 
 				<Route
@@ -46,28 +39,17 @@ export default function AppGateway() {
 								users={users}
 								post={post}
 								user={user}
-								API={API}
 							/>
 						</ProtectedRoute>
 					}
 				/>
 
-				<Route path='/addpost' element={<New />} />
-
 				<Route path='/contact' element={<div>contact page</div>} />
-				<Route
-					path='/messenger'
-					element={<div>This part of application is under construction</div>}
-				/>
-				<Route
-					path='/friends'
-					element={
-						<div>
-							This part of application should be able to interact between users
-						</div>
-					}
-				/>
 			</Routes>
 		</BrowserRouter>
 	);
 }
+
+
+
+
--- src/components/login/Login 2.js
+++ src/components/login/Login 2.js
@@ -2,26 +2,22 @@ import React from "react";
 import { useState } from "react";
 import Newuser from "./Signup.js";
 import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
-export default function Signup({ setUser, setPermission }) {
-	// navigate will navigate to the route assigned in argument
+import { useNavigate } from "react-router-dom";
+export default function Login({ setUser,setPermission }) {
+	// will validate the user and get user id
+	// map through the users and get the specific user data
+	//
+
 	const navigate = useNavigate();
-	// renaming for easier access to the function
-
 	const API = "https://secure-forest-62515.herokuapp.com/api";
-	// var for api declared so it can be changed easily for the whole app
-
 	const [username, setUsername] = useState(" ");
 	const [password, setPassword] = useState(" ");
-	const [password2, setPassword2] = useState(" ");
+	const [signUp, setShowSignUp] = useState(false);
 
-	const [vision, setVision] = useState(false);
-
-	const passwordToggle = () => {
-		vision ? setVision(false) : setVision(true);
+	const showSignUp = () => {
+		setShowSignUp(true);
 	};
 
-	// this function will store the value to the state
 	const handleUsername = (event) => {
 		setUsername(event.target.value);
 	};
@@ -29,26 +25,29 @@ export default function Signup({ setUser, setPermission }) {
 	const handlePassword = (event) => {
 		setPassword(event.target.value);
 	};
-	const handlePassword2 = (event) => {
-		setPassword2(event.target.value);
-	};
 
-	// this function is to Signin
-	const handleSignup = (username, password) => {
-		// axios post request to send signin credentials to the backend
-		if (password === password2) {
-			axios
-				.post(`${API}/users`, {
-					username: username,
-					password: password,
-					//  value in backend : value from react app
-				})
-				.then((response) => {
-					navigate("/login");
-				});
-		} else {
-			alert("Passwords does not match");
-		}
+	const handleLogin = (username, password) => {
+		axios
+			.put(`${API}/users/login`, {
+				username: username,
+				password: password,
+			})
+			.catch((error) => {
+				if (error) {
+					alert("Email or password does not match records");
+				}
+			})
+			.then((response) => {
+                setUser(response.data);
+				console.log(response.data);
+				if (Object.keys(response.data).length === 0) {
+					console.log("something went wrong");
+                } else {
+                    setPermission(true)
+					navigate("/");
+					// pageToggle();
+				}
+			});
 	};
 
 	return (
@@ -56,17 +55,19 @@ export default function Signup({ setUser, setPermission }) {
 			<section className='bg-yellow-300  relative flex flex-wrap lg:h-screen lg:items-center'>
 				<div className='w-full px-4 py-12 lg:w-1/2 sm:px-6 lg:px-8 sm:py-16 lg:py-24'>
 					<div className='max-w-lg mx-auto text-center'>
-						<img className='object' src='images/logo.png' />
-						<h3 className='text-xl font-bold sm:text-3xl'>
+						<h1 className='text-2xl font-bold sm:text-3xl'>
 							Get started today!
-						</h3>
+						</h1>
 
-						<p className='mt-4 text-gray-500'>Are you Yello?</p>
+						<p className='mt-4 text-gray-500'>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Et libero
+							nulla eaque error neque ipsa culpa autem, at itaque nostrum!
+						</p>
 					</div>
 					<form
 						onSubmit={(event) => {
 							event.preventDefault();
-							handleSignup(username, password);
+							handleLogin(username, password);
 						}}
 						className='max-w-md mx-auto mt-8 mb-0 space-y-4'
 					>
@@ -140,64 +141,22 @@ export default function Signup({ setUser, setPermission }) {
 								</span>
 							</div>
 						</div>
-						<div>
-							<label htmlFor='password' className='sr-only'>
-								Password
-							</label>
-							<div className='relative'>
-								<input
-									name='password'
-									type={` ${vision ? "text" : "password"}`}
-									className='w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm'
-									placeholder='Confirm password'
-									onChange={handlePassword2}
-								/>
-
-								<span
-									onClick={() => passwordToggle()}
-									className=' cursor-pointer absolute inset-y-0 inline-flex items-center right-4'
-								>
-									<svg
-										xmlns='http://www.w3.org/2000/svg'
-										className='w-5 h-5 text-gray-400'
-										fill='none'
-										viewBox='0 0 24 24'
-										stroke='currentColor'
-									>
-										<path
-											strokeLinecap='round'
-											strokeLinejoin='round'
-											strokeWidth='2'
-											d='M15 12a3 3 0 11-6 0 3 3 0 016 0z'
-										/>
-										<path
-											strokeLinecap='round'
-											strokeLinejoin='round'
-											strokeWidth='2'
-											d='M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z'
-										/>
-									</svg>
-								</span>
-							</div>
-						</div>
 
 						<div className='flex items-center justify-between'>
-							<p className='text-sm text-gray-500'>
-								<Link type='submit' className='underline' to={`/login`}>
-									Login
-								</Link>
-							</p>
-
-							<div className='flex items-center justify-between'>
-								<button
-									type='submit'
-									className='bg-lime-500 inline-block px-5 py-3 ml-3 text-sm font-lg  rounded-lg'
-								>
-									Sign up
-								</button>
-							</div>
+							<button
+								type='submit'
+								className='bg-lime-500 inline-block px-5 py-3 ml-3 text-sm font-lg  rounded-lg'
+							>
+								Sign in
+							</button>
 						</div>
 					</form>
+					<div>
+						<button className='underline' onClick={showSignUp}>
+							Sign up
+							{signUp ? <Newuser /> : null}
+						</button>
+					</div>
 				</div>
 
 				<div className='relative w-full h-64 sm:h-96 lg:w-1/2 lg:h-full'>
